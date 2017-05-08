d6f3eca9ccda556c7fa168c9bb72d50d4179c6e1
praveen <praveen@gamutgurus.com>
2017-05-08 20:03:55 +0530

    Remvoe ANT run project

--- AntProject-master/build.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-ant -buildfile jbuild.xml main
-IF ERRORLEVEL 1 (
-	echo "Failed build"
-	exit 1
-)
--- AntProject-master/testSA.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
- 
-echo "Build PMD"
-ant  -buildfile jbuild.xml pmd
-IF ERRORLEVEL 1 (
-	echo "Failed on testSA"
-	exit 1
-)
